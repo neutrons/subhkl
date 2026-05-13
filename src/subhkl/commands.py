16f2779dd3ae04a02418dfb1f080fd1c25fb158c
Jens Glaser <glaserj@ornl.gov>
2026-05-13 05:40:55 -0400

    Revert "broadcast instead of softmax for O(1) assgnmnt"
    This reverts commit 37fd72c6de4a12ededb252ecd31ce13067c1d62c.

--- src/subhkl/commands.py
+++ src/subhkl/commands.py
@@ -1736,9 +1736,12 @@ def run_bingham_tracker(
             A_ensemble_state, bg_hist_ensemble, wl_hist_ensemble, q_batch, ki_batch, t_batch, current_sigma_q, delta_angle
         )
 
-        # broadcast best histogram
-        best_idx = jnp.argmin(loss_ensemble)
-        wl_hist_ensemble = jnp.broadcast_to(wl_hist_ensemble[best_idx], wl_hist_ensemble.shape)
+        # Convert NLL losses to probability weights
+        ensemble_weights = jax.nn.softmax(-loss_ensemble)
+
+        # Take a weighted average of the histograms
+        wl_hist_weighted = jnp.sum(wl_hist_ensemble * ensemble_weights[:, None], axis=0)
+        wl_hist_ensemble = jnp.broadcast_to(wl_hist_weighted, wl_hist_ensemble.shape)
 
         best_idx = int(jnp.argmin(loss_ensemble))
         U_best = np.array(U_ensemble_curr[best_idx])
